2d5fb0a69f605a7f8ba128a51c2ef6ea0ebd247d
wdesmonde <willazen@gmail.com>
2012-10-20 13:11:01 -0700

    changed title of show all page

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '3.1.0'
 
 gem 'sqlite3'
 
-gem 'therubyracer'
+#gem 'therubyracer'
 gem 'execjs'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013194817) do
+ActiveRecord::Schema.define(:version => 20121014170346) do
 
   create_table "stories", :force => true do |t|
     t.string   "as_a"
