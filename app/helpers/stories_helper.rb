f1ead5e60fb9b8b3d40aeffccb5691d4b1bcb0bc
Paul Munro <pmbizmail@gmail.com>
2013-05-05 16:12:17 -0700

    Display tags and story ID for each story on the index page.  The tags are clickable links. Underline clickable links on each story line on the index page upon hover.  The display of all tag lists, including the tag cloud, are now sorted alphabetically.

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,3 +1,7 @@
 module StoriesHelper
 
+  def tag_link_list (story_param)
+    tag_set = story_param.tags.sort_by { |f| f.name }
+    tag_set.map { |t| link_to t.name, tag_path(t.name) }.join(', ')
+  end
 end
